aa9a94228bfa3f01588cd5ecc69aef458590fe82
Antonio Carlos Lima <limaantoniocarlos20@gmail.com>
2021-02-25 02:12:13 -0300

    first commit

---
+++ src/main/domain/User.ts
@@ -0,0 +1,13 @@
+class User {
+  id: String;
+  name: String;
+  image: String
+
+  constructor(id: string, name: string, image: string){
+    this.id = id,
+    this.name = name,
+    this.image = image
+  } 
+}
+
+export default User;
